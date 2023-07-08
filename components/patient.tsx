db71e9d3a6fe2ece503a2f1aa84ece233bfd3d97
Frank Cairampoma <ingfralch@gmail.com>
2023-07-08 12:52:22 -0500

    diseño pacienta done

--- components/patient.tsx
+++ components/patient.tsx
@@ -5,16 +5,41 @@ import {
     Modal, Alert, Linking, Touchable, Dimensions
 } from 'react-native';
 import { storeSesion, getSesion, removeSesion } from '../hooks/handleSession';
-import { Entypo, FontAwesome, FontAwesome5, Foundation } from '@expo/vector-icons';
+import { Entypo, FontAwesome, FontAwesome5, AntDesign } from '@expo/vector-icons';
 import BottonNavigationBar from './bottonNavigationBar';
 
 export default function Patient() {
     const ancho = Dimensions.get('window').width;
-   
+
     const list_datos_body = [
         ['Jose Sanchez Lopez', '02-03-2023'],
         ['Andres Meza Rojas', '02-03-2023'],
         ['Ana Mendoza Diaz', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
+        ['Jose Sanchez Lopez', '02-03-2023'],
         ['Jose Sanchez Lopez', '02-03-2023'],
     ]
 
@@ -34,28 +59,30 @@ export default function Patient() {
                     >
                     </TouchableOpacity>
                 </View>
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={{ fontSize: 20, color: '#fff' }}>Pacientes</Text>
-                    {
-                        list_datos_body.map((item, index) => (
-                            <View key={index} style={{ width: ancho - 20, height: 70, backgroundColor: '#555', borderRadius: 10, marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                                <View style={{ width: '70%', height: '100%', justifyContent: 'center', marginLeft: 10 }}>
-                                    <Text style={{ fontSize: 16, fontWeight: 'bold', color:"#fff" }}>{item[0]}</Text>
-                                    <Text style={{ fontSize: 14 , color:"#fff"}}>{item[1]}</Text>
+                <ScrollView>
+                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', }}>
+                        {
+                            list_datos_body.map((item, index) => (
+                                <View key={index} style={{ width: ancho - 20, height: 70, backgroundColor: '#555', borderRadius: 10, marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                                    <View style={{ width: '70%', height: '100%', justifyContent: 'center', marginLeft: 10 }}>
+                                        <Text style={{ fontSize: 16, fontWeight: 'bold', color: "#fff" }}>{item[0]}</Text>
+                                        <Text style={{ fontSize: 14, color: "#fff" }}>Ultima visita:  {item[1]}</Text>
+                                    </View>
+                                    <View style={{ width: '30%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                                        <TouchableOpacity >
+                                            <AntDesign name="rightcircle" size={25} color="#333" />
+                                        </TouchableOpacity>
+                                    </View>
                                 </View>
-                                <View style={{ width: '30%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-                                    <TouchableOpacity style={{ width: 50, height: 50, borderRadius: 50, backgroundColor: '#444', alignItems: 'center', justifyContent: 'center' }}>
-                                        <FontAwesome name="eye" size={24} color="#fff" />
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                        ))
+                            ))
 
 
 
-                    }
+                        }
 
-                </View>
+                    </View>
+                </ScrollView>
+
             </View>
             <BottonNavigationBar ruta='Patient' />
         </View>
@@ -69,10 +96,12 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         paddingVertical: 20,
         backgroundColor: '#444',
+
     },
     cuerpo: {
         flex: 1,
         width: '100%',
+
     },
-    text: { margin: 6 , color: '#fff'},
+    text: { margin: 6, color: '#fff' },
 });
