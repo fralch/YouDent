0a7cf17a1983634b655af77501c60716a91ce5e9
Frank Cairampoma <ingfralch@gmail.com>
2023-07-18 15:14:28 -0500

    modificando tamaño de dientes

--- components/tooth.tsx
+++ components/tooth.tsx
@@ -16,64 +16,64 @@ export default function Tooth() {
         <View style={styles.container}>
             <View style={styles.cuerpo}>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d1.png')} style={{ height: 25, width: 75 }} />
-                    <Image source={require('../assets/tooth/d2.png')} style={{ height: 25, width: 25 }} />
-                    <Image source={require('../assets/tooth/d3.png')} style={{ height: 25, width: 25 }} />
-                    <Image source={require('../assets/tooth/d4.png')} style={{ height: 25, width: 75 }} />
+                    <Image source={require('../assets/tooth/d1.png')} style={{ height: 45, width: 118 }} />
+                    <Image source={require('../assets/tooth/d2.png')} style={{ height: 40, width: 40 }} />
+                    <Image source={require('../assets/tooth/d3.png')} style={{ height: 40, width: 40 }} />
+                    <Image source={require('../assets/tooth/d4.png')} style={{  height: 45, width: 118  }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d5.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d6.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d5.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d6.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d7.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d8.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d7.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d8.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d9.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d10.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d9.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d10.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d11.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d12.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d11.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d12.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d13.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d14.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d13.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d14.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d15.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d16.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d15.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d16.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" , marginTop:20}}>
-                    <Image source={require('../assets/tooth/d17.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d18.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d17.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d18.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d19.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d20.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d19.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d20.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d21.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d22.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d21.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d22.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row" }}>
-                    <Image source={require('../assets/tooth/d23.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d24.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d23.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d24.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row"}}>
-                    <Image source={require('../assets/tooth/d25.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d26.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d25.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d26.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row"}}>
-                    <Image source={require('../assets/tooth/d27.png')} style={{ height: 25, width: 100 }} />
-                    <Image source={require('../assets/tooth/d28.png')} style={{ height: 25, width: 100 }} />
+                    <Image source={require('../assets/tooth/d27.png')} style={{ height: 35, width: 170 }} />
+                    <Image source={require('../assets/tooth/d28.png')} style={{ height: 35, width: 170 }} />
                 </View>
                 <View style={{ flexDirection: "row"}}>
-                    <Image source={require('../assets/tooth/d29.png')} style={{ height: 25, width: 75 }} />
-                    <Image source={require('../assets/tooth/d30.png')} style={{ height: 25, width: 25 }} />
-                    <Image source={require('../assets/tooth/d31.png')} style={{ height: 25, width: 25 }} />
-                    <Image source={require('../assets/tooth/d32.png')} style={{ height: 25, width: 75 }} />
+                    <Image source={require('../assets/tooth/d29.png')} style={{  height: 45, width: 118  }} />
+                    <Image source={require('../assets/tooth/d30.png')} style={{ height: 40, width: 40 }}  />
+                    <Image source={require('../assets/tooth/d31.png')} style={{ height: 40, width: 40 }}  />
+                    <Image source={require('../assets/tooth/d32.png')} style={{  height: 45, width: 118  }} />
                 </View>
             </View>
 
